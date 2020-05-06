10e26cfa9f08794aaeb495f53c0a5cda6633f38a
任鹏辉 <renpenghui@beadwallet.com>
2020-05-06 13:13:55 +0800

    init

---
+++ com/mcom/core/sms/domain/entity/User.java
@@ -0,0 +1,275 @@
+package com.mcom.core.sms.domain.entity;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String headUrl;
+
+    private String phone;
+
+    private String phoneCode;
+
+    private String email;
+
+    private Byte sex;
+
+    private Byte age;
+
+    private String idCard;
+
+    private String panCard;
+
+    private String panIssueDate;
+
+    private String lon;
+
+    private String lat;
+
+    private String province;
+
+    private String city;
+
+    private String district;
+
+    private String registerAddr;
+
+    private String mobileSeq;
+
+    private String inviteCode;
+
+    private Long inviteId;
+
+    private Long channelId;
+
+    private String remark;
+
+    private Byte status;
+
+    private Byte deleted;
+
+    private Date gmtCreated;
+
+    private Date gmtModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl == null ? null : headUrl.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getPhoneCode() {
+        return phoneCode;
+    }
+
+    public void setPhoneCode(String phoneCode) {
+        this.phoneCode = phoneCode == null ? null : phoneCode.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard == null ? null : idCard.trim();
+    }
+
+    public String getPanCard() {
+        return panCard;
+    }
+
+    public void setPanCard(String panCard) {
+        this.panCard = panCard == null ? null : panCard.trim();
+    }
+
+    public String getPanIssueDate() {
+        return panIssueDate;
+    }
+
+    public void setPanIssueDate(String panIssueDate) {
+        this.panIssueDate = panIssueDate == null ? null : panIssueDate.trim();
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon == null ? null : lon.trim();
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat == null ? null : lat.trim();
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province == null ? null : province.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district == null ? null : district.trim();
+    }
+
+    public String getRegisterAddr() {
+        return registerAddr;
+    }
+
+    public void setRegisterAddr(String registerAddr) {
+        this.registerAddr = registerAddr == null ? null : registerAddr.trim();
+    }
+
+    public String getMobileSeq() {
+        return mobileSeq;
+    }
+
+    public void setMobileSeq(String mobileSeq) {
+        this.mobileSeq = mobileSeq == null ? null : mobileSeq.trim();
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode == null ? null : inviteCode.trim();
+    }
+
+    public Long getInviteId() {
+        return inviteId;
+    }
+
+    public void setInviteId(Long inviteId) {
+        this.inviteId = inviteId;
+    }
+
+    public Long getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(Long channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Byte getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Byte deleted) {
+        this.deleted = deleted;
+    }
+
+    public Date getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Date gmtCreated) {
+        this.gmtCreated = gmtCreated;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
---
+++ src/test/java/com/sunrise/base/service/TestMyBatisGeneratorTool.java
@@ -0,0 +1,63 @@
+package com.sunrise.base.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.util.ResourceUtils;
+
+public class TestMyBatisGeneratorTool {
+
+	@Test
+	public void generate() {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = null;
+		try {
+
+			configFile = ResourceUtils.getFile("classpath:generatorConfig-forum.xml");
+
+
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		System.out.println(configFile.getAbsolutePath());
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = null;
+		try {
+			config = cp.parseConfiguration(configFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (XMLParserException e) {
+			e.printStackTrace();
+		}
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = null;
+		try {
+			myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		} catch (InvalidConfigurationException e) {
+			e.printStackTrace();
+		}
+		try {
+			myBatisGenerator.generate(null);
+			System.out.println("finished!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
